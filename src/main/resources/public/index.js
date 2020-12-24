f05a236415b9d4aadbad7955570ebe5f915b771b
梅思铭 <281780862@qq.com>
2020-12-25 04:54:51 +0800

    新增注释

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,9 +1,21 @@
 const Y = {
+  /**
+   * api 路径
+   */
   api: location.origin,
+  /**
+   * GET 方法
+   * @param url {string} url路径
+   * @param callback {Function} 回调函数
+   */
   get(url, callback) {
     fetch(Y.api + '/' + url)
         .then(response => response.json())
         .then(callback)
         .catch(() => alert('加载失败'));
+  },
+
+  refreshFileList(url) {
+
   }
 }
